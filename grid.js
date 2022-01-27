b169d109f55002a1c9656de3081bdf1e8506026c
Durga <mdprasadeng@gmail.com>
2022-01-28 00:05:50 +0530

    sudoku generator added

---
+++ grid.js
@@ -0,0 +1,108 @@
+var ONE = 1;
+var TWO = 2;
+var THREE = 3;
+var FOUR = 4;
+var FIVE = 5;
+var SIX = 6;
+var SEVEN = 7;
+var EIGHT = 8;
+var NINE = 9;
+
+var ALL = ONE | TWO | THREE | FOUR | FIVE | SIX | SEVEN | EIGHT | NINE;
+var ALL_ARRAY = [ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE];
+
+var blackListed = [];
+var filled = [];
+var fillHistory = [];
+function grid() {
+    for (let i = 0; i < 9; i++) {
+        blackListed[i] = [];
+        filled[i] = [];
+        for (let j = 0; j < 9; j++) {
+            blackListed[i][j] = [];
+            filled[i][j] = -1;
+        }
+    }
+
+    var x = 0, y = 0;
+    while (1) {
+        var options = GetOptions(x, y);
+        //console.log("OP at x:" + x + "y:" + y + "items:" + options);
+
+        if (options.length == 0) {
+            if (fillHistory.length == 0) {
+                console.warn("No history to go back");
+                break;
+            }
+            blackListed[x][y] = [];
+            var lastEntry = fillHistory.pop();
+            x = lastEntry.x;
+            y = lastEntry.y;
+            blackListed[x][y].push(filled[x][y]);
+            filled[x][y] = -1;
+            //console.log("BL at x:" + x + "y:" + y + "items:" + blackListed[x][y]);
+            continue;
+        }
+        var option = options[Math.floor(Math.random() * options.length)];
+        filled[x][y] = option;
+        //console.log("FL at x:" + x + "y:" + y + "items:" + filled[x][y]);
+        fillHistory.push({x: x, y:y});
+        if (x == 8) {
+            if (y == 8) {
+                console.log("Sudoku done");
+                break;
+            } else {
+                y++;
+                x = 0;
+                continue;
+            }
+        } else {
+            x++;
+        } 
+    }
+
+    for (let i = 0; i < 9; i++) {
+        console.log(filled[i].join("|"))
+    }
+    return filled;
+}
+
+function GetOptions(x, y) {
+    var options = {};
+    ALL_ARRAY.forEach(e => {
+        options[e] = 1;
+    })
+
+
+    for (let i = 0; i < 9; i++) {
+        if (filled[x][i] > 0) {
+            options[filled[x][i]] = 0;
+        }
+        if (filled[i][y] > 0) {
+            options[filled[i][y]] = 0;
+        }
+    }
+
+    var cx = x - x % 3;
+    var cy = y - y % 3;
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (filled[cx+i][cy+j] > 0) {
+                options[filled[cx+i][cy+j]] = 0;
+            }
+        }
+    }
+    blackListed[x][y].forEach(e => {
+        options[e] = 0;
+    })
+
+    var values = [];
+    ALL_ARRAY.forEach(e => {
+        if (options[e] == 1) {
+            values.push(e);
+        }
+    });
+    return values;
+}
+
--- sudokle.js
+++ sudokle.js
@@ -1,16 +1,10 @@
 var state = {
-    width: 360*2,
-    height: 360*2,
+    width: 360 * 2,
+    height: 360 * 2,
     unitSize: 80
 };
 
-var cell = [];
-for (let i=0; i < 9; i++) {
-    cell[i] =[];
-    for (let j=0; j < 9; j++) {
-        cell[i][j] = (i + j ) % 9 + 1;
-    }
-}    
+var cell = grid();
 
 function onLoad() {
     var canvas = document.getElementById('playarea');
@@ -24,32 +18,32 @@ function onLoad() {
 
 function drawSudoku(ctx) {
     for (let i = 0; i <= 9; i++) {
-        if (i==0 || i==9) {
-            ctx.lineWidth = 10;    
+        if (i == 0 || i == 9) {
+            ctx.lineWidth = 10;
         }
-        else if (i==3 || i ==6) {
-            ctx.lineWidth = 3;    
+        else if (i == 3 || i == 6) {
+            ctx.lineWidth = 3;
         } else {
-            ctx.lineWidth = 1;    
+            ctx.lineWidth = 1;
         }
-         
-        ctx.beginPath();       
-        ctx.moveTo(state.unitSize*i, 0);    
-        ctx.lineTo(state.unitSize*i, state.height);  
+
+        ctx.beginPath();
+        ctx.moveTo(state.unitSize * i, 0);
+        ctx.lineTo(state.unitSize * i, state.height);
         ctx.stroke();
     }
     for (let i = 0; i <= 9; i++) {
-        if (i==0 || i==9) {
-            ctx.lineWidth = 10;    
+        if (i == 0 || i == 9) {
+            ctx.lineWidth = 10;
         }
-        else if (i==3 || i ==6) {
-            ctx.lineWidth = 3;    
+        else if (i == 3 || i == 6) {
+            ctx.lineWidth = 3;
         } else {
-            ctx.lineWidth = 1;    
+            ctx.lineWidth = 1;
         }
-        ctx.beginPath();  
-        ctx.moveTo(0, state.unitSize*i, 0);    
-        ctx.lineTo(state.width, state.unitSize*i);  
+        ctx.beginPath();
+        ctx.moveTo(0, state.unitSize * i, 0);
+        ctx.lineTo(state.width, state.unitSize * i);
         ctx.stroke();
     }
 
@@ -57,56 +51,60 @@ function drawSudoku(ctx) {
     ctx.lineWidth = diagWidth;
     // top left 1
     for (let i = 0; i < 9; i++) {
-        ctx.beginPath(); 
-        ctx.moveTo(0, state.unitSize/2 + state.unitSize*i );
-        ctx.lineTo(state.unitSize/2 + state.unitSize*i, 0);
+        ctx.beginPath();
+        ctx.moveTo(0, state.unitSize / 2 + state.unitSize * i);
+        ctx.lineTo(state.unitSize / 2 + state.unitSize * i, 0);
         ctx.stroke();
     }
 
     // top left 2
     for (let i = 0; i < 9; i++) {
-        ctx.beginPath(); 
-        ctx.moveTo(state.unitSize/2 + state.unitSize*i, state.height );
-        ctx.lineTo(state.width, state.unitSize/2 + state.unitSize*i);
+        ctx.beginPath();
+        ctx.moveTo(state.unitSize / 2 + state.unitSize * i, state.height);
+        ctx.lineTo(state.width, state.unitSize / 2 + state.unitSize * i);
         ctx.stroke();
     }
 
     // top right 1
     for (let i = 0; i < 9; i++) {
-        ctx.beginPath(); 
-        ctx.moveTo(state.unitSize/2 + state.unitSize*i ,0);
-        ctx.lineTo(state.width, state.unitSize/2 + state.unitSize*(9-1 -i));
+        ctx.beginPath();
+        ctx.moveTo(state.unitSize / 2 + state.unitSize * i, 0);
+        ctx.lineTo(state.width, state.unitSize / 2 + state.unitSize * (9 - 1 - i));
         ctx.stroke();
     }
 
     //top right 2
     for (let i = 0; i < 9; i++) {
-        ctx.beginPath(); 
-        ctx.moveTo(0, state.unitSize/2 + state.unitSize*i);
-        ctx.lineTo(state.unitSize/2 + state.unitSize*(9-1 -i), state.height);
+        ctx.beginPath();
+        ctx.moveTo(0, state.unitSize / 2 + state.unitSize * i);
+        ctx.lineTo(state.unitSize / 2 + state.unitSize * (9 - 1 - i), state.height);
         ctx.stroke();
     }
 
     var fontSize = 30;
-    ctx.font = fontSize+'px monospace';
+    ctx.font = fontSize + 'px monospace';
     ctx.textBaseline = 'middle';
-    ctx.textAlign = "center";  
+    ctx.textAlign = "center";
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
-            ctx.fillText(cell[i][j], state.unitSize/2 + state.unitSize*i, state.unitSize/2 + state.unitSize*j );
+            ctx.fillText(cell[i][j], state.unitSize / 2 + state.unitSize * i, state.unitSize / 2 + state.unitSize * j);
         }
     }
 
     var fontSize = 12;
-    ctx.font = fontSize+'px monospace';
+    ctx.font = fontSize + 'px monospace';
     for (let i = 0; i < 9; i++) {
         for (let j = 0; j < 9; j++) {
-            ctx.fillText(cell[i][j], state.unitSize/2 + state.unitSize*i - state.unitSize/3, state.unitSize/2 + state.unitSize*j - state.unitSize/3);
-            ctx.fillText(cell[i][j], state.unitSize/2 + state.unitSize*i - state.unitSize/3, state.unitSize/2 + state.unitSize*j + state.unitSize/3);
-            ctx.fillText(cell[i][j], state.unitSize/2 + state.unitSize*i + state.unitSize/3, state.unitSize/2 + state.unitSize*j - state.unitSize/3);
-            ctx.fillText(cell[i][j], state.unitSize/2 + state.unitSize*i + state.unitSize/3, state.unitSize/2 + state.unitSize*j + state.unitSize/3);
+            // ctx.fillText(cell[i][j], state.unitSize / 2 + state.unitSize * i - state.unitSize / 3, state.unitSize / 2 + state.unitSize * j - state.unitSize / 3);
+            // ctx.fillText(cell[i][j], state.unitSize / 2 + state.unitSize * i - state.unitSize / 3, state.unitSize / 2 + state.unitSize * j + state.unitSize / 3);
+            // ctx.fillText(cell[i][j], state.unitSize / 2 + state.unitSize * i + state.unitSize / 3, state.unitSize / 2 + state.unitSize * j - state.unitSize / 3);
+            // ctx.fillText(cell[i][j], state.unitSize / 2 + state.unitSize * i + state.unitSize / 3, state.unitSize / 2 + state.unitSize * j + state.unitSize / 3);
 
         }
     }
+
+}
+
+function GenSudoku() {
 
 }
